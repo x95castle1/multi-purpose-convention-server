b744e86b7b18915f74d1c0227e750c8f4890f4e5
Jeremy Castle <x95castle1@yahoo.com>
2023-09-18 15:30:47 -0500

    Updated Tests to Run with 60% coverage...

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 require (
 	github.com/CycloneDX/cyclonedx-go v0.7.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/go-cmp v0.5.9
 	github.com/google/go-containerregistry v0.16.1 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -24,7 +25,7 @@ require (
 	golang.org/x/text v0.13.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/apimachinery v0.28.2 // indirect
+	k8s.io/apimachinery v0.28.2
 	k8s.io/klog/v2 v2.100.1 // indirect
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
 	sigs.k8s.io/controller-runtime v0.16.2 // indirect
---
+++ pkg/conventions/conventions_test.go
@@ -0,0 +1,252 @@
+package conventions
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+
+	webhookv1alpha1 "github.com/vmware-tanzu/cartographer-conventions/webhook/api/v1alpha1"
+	"github.com/x95castle1/convention-server-framework/pkg/handler"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/vmware-tanzu/cartographer-conventions/webhook"
+	"go.uber.org/zap"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const imageDefault = "sample-accelerators/tanzu-java-web-app"
+
+func Test_addConventions(t *testing.T) {
+	testdataPath := "testdata"
+	var emptyAppliedConventions []string
+	l := zap.NewNop().Sugar()
+	type args struct {
+		logger   *zap.SugaredLogger
+		template *corev1.PodTemplateSpec
+		images   []webhook.ImageConfig
+	}
+	tests := []struct {
+		skip               bool
+		name               string
+		args               args
+		want               []string
+		wantErr            bool
+		validateTemplate   bool
+		wantedTemplateFile string
+	}{
+		{
+			name: "no images",
+			args: args{
+				logger:   l,
+				template: getMockTemplate(""),
+				images:   make([]webhook.ImageConfig, 0),
+			},
+			want:    emptyAppliedConventions,
+			wantErr: false,
+		},
+		{
+			name: "readinessProbe",
+			args: args{
+				logger:   l,
+				template: getMockTemplateWithImageAndAnnotation("", fmt.Sprintf("%s/readinessProbe", Prefix), "{\"exec\":{\"command\":[\"cat\",\"/tmp/healthy\"]},\"initialDelaySeconds\":5,\"periodSeconds\":5}"),
+				images: []webhook.ImageConfig{
+					{
+						Image: imageDefault,
+						BOMs: []webhookv1alpha1.BOM{
+							{
+								Name: "cnb-app:dependencies",
+								Raw:  getFileBytes(testdataPath + "/boms/bom.cdx.not_springboot.json"),
+							},
+						},
+					},
+				},
+			},
+			want:               []string{fmt.Sprintf("%s-readiness", Prefix)},
+			wantErr:            false,
+			validateTemplate:   true,
+			wantedTemplateFile: "readinessProbe.json",
+		},
+		{
+			name: "livenessProbe",
+			args: args{
+				logger:   l,
+				template: getMockTemplateWithImageAndAnnotation("", fmt.Sprintf("%s/livenessProbe", Prefix), "{\"exec\":{\"command\":[\"cat\",\"/tmp/healthy\"]},\"initialDelaySeconds\":5,\"periodSeconds\":5}"),
+				images: []webhook.ImageConfig{
+					{
+						Image: imageDefault,
+						BOMs: []webhookv1alpha1.BOM{
+							{
+								Name: "cnb-app:dependencies",
+								Raw:  getFileBytes(testdataPath + "/boms/bom.cdx.not_springboot.json"),
+							},
+						},
+					},
+				},
+			},
+			want:               []string{fmt.Sprintf("%s-liveness", Prefix)},
+			wantErr:            false,
+			validateTemplate:   true,
+			wantedTemplateFile: "livenessProbe.json",
+		},
+		{
+			name: "startupProbe",
+			args: args{
+				logger:   l,
+				template: getMockTemplateWithImageAndAnnotation("", fmt.Sprintf("%s/startupProbe", Prefix), "{\"exec\":{\"command\":[\"cat\",\"/tmp/healthy\"]},\"initialDelaySeconds\":5,\"periodSeconds\":5}"),
+				images: []webhook.ImageConfig{
+					{
+						Image: imageDefault,
+						BOMs: []webhookv1alpha1.BOM{
+							{
+								Name: "cnb-app:dependencies",
+								Raw:  getFileBytes(testdataPath + "/boms/bom.cdx.not_springboot.json"),
+							},
+						},
+					},
+				},
+			},
+			want:               []string{fmt.Sprintf("%s-startup", Prefix)},
+			wantErr:            false,
+			validateTemplate:   true,
+			wantedTemplateFile: "startupProbe.json",
+		},
+		{
+			name: "args",
+			args: args{
+				logger:   l,
+				template: getMockTemplateWithImageAndAnnotation("", fmt.Sprintf("%s/args", Prefix), "[\"one\",\"two\",\"three\"]"),
+				images: []webhook.ImageConfig{
+					{
+						Image: imageDefault,
+						BOMs: []webhookv1alpha1.BOM{
+							{
+								Name: "cnb-app:dependencies",
+								Raw:  getFileBytes(testdataPath + "/boms/bom.cdx.not_springboot.json"),
+							},
+						},
+					},
+				},
+			},
+			want:               []string{fmt.Sprintf("%s-args", Prefix)},
+			wantErr:            false,
+			validateTemplate:   true,
+			wantedTemplateFile: "args.json",
+		},
+		{
+			name: "storage",
+			args: args{
+				logger:   l,
+				template: getMockTemplateWithImageAndAnnotation("", fmt.Sprintf("%s/storage", Prefix), "{\"volumeMounts\":[{\"mountPath\":\"/test\",\"name\":\"test\"}],\"volumes\":[{\"name\":\"test\",\"emptyDir\":{}}]}"),
+				images: []webhook.ImageConfig{
+					{
+						Image: imageDefault,
+						BOMs: []webhookv1alpha1.BOM{
+							{
+								Name: "cnb-app:dependencies",
+								Raw:  getFileBytes(testdataPath + "/boms/bom.cdx.not_springboot.json"),
+							},
+						},
+					},
+				},
+			},
+			want:               []string{fmt.Sprintf("%s-storage", Prefix)},
+			wantErr:            false,
+			validateTemplate:   true,
+			wantedTemplateFile: "storage.json",
+		},
+	}
+
+	for _, tt := range tests {
+		if tt.skip {
+			t.Logf("skipping test %s", tt.name)
+			continue
+		}
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := handler.AddConventions(tt.args.logger, tt.args.template, tt.args.images, Conventions)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AddConventions() = unwanted error: %v", err)
+				return
+			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("%s() = (-expected, +actual): %s", tt.name, diff)
+			}
+
+			if tt.validateTemplate {
+				if tt.wantedTemplateFile == "" {
+					t.Errorf("AddConventions(): [%s] Template JSON file not provided", tt.name)
+				} else {
+					pts := &corev1.PodTemplateSpec{}
+					tf := getFileBytes(testdataPath + "/templates/" + tt.wantedTemplateFile)
+
+					err = json.Unmarshal(tf, pts)
+					if err != nil {
+						t.Errorf("AddConventions(): [%s] %v", tt.name, err)
+					}
+					if diff := cmp.Diff(tt.args.template, pts); diff != "" {
+						t.Errorf("%s() = (-expected, +actual): %s", tt.name, diff)
+					}
+				}
+			}
+		})
+	}
+}
+
+func getFileBytes(file string) []byte {
+	b, _ := os.ReadFile(file)
+	return b
+}
+
+func getMockTemplate(img string) *corev1.PodTemplateSpec {
+	if img == "" {
+		img = imageDefault
+	}
+	return &corev1.PodTemplateSpec{
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "workload",
+					Image: img,
+					Ports: make([]corev1.ContainerPort, 0),
+				},
+			},
+		},
+	}
+}
+
+func getMockTemplateWithImageAndAnnotation(img string, key string, value string) *corev1.PodTemplateSpec {
+	if img == "" {
+		img = imageDefault
+	}
+	return &corev1.PodTemplateSpec{
+		ObjectMeta: v1.ObjectMeta{Annotations: map[string]string{key: value}},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "workload",
+					Image: img,
+					Ports: make([]corev1.ContainerPort, 0),
+				},
+			},
+		},
+	}
+}
+
+func getMockTemplateWithImageAndLabel(img string, key string, value string) *corev1.PodTemplateSpec {
+	if img == "" {
+		img = imageDefault
+	}
+	return &corev1.PodTemplateSpec{
+		ObjectMeta: v1.ObjectMeta{Labels: map[string]string{key: value}},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "workload",
+					Image: img,
+					Ports: make([]corev1.ContainerPort, 0),
+				},
+			},
+		},
+	}
+}
